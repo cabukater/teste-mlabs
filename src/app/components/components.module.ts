461becab63b88d0ff962fc7de72775ddbcde82a0
cabukater <cabukater@gmail.com>
2021-01-20 15:36:00 -0300

    css e estrutura da pagina home

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports:[HeaderComponent],
-  imports: [
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],exports:[
+    HeaderComponent,
+    FooterComponent
+  ],imports: [
     CommonModule
   ]
 })
