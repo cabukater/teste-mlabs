fadce692f1127ec745dd34d43796b20d5fefa260
cabukater <cabukater@gmail.com>
2021-01-25 20:18:13 -0300

    corrigido datepicker

--- src/app/agendamento/agendamento.component.ts
+++ src/app/agendamento/agendamento.component.ts
@@ -21,11 +21,11 @@ export class AgendamentoComponent implements OnInit {
   toggled: boolean = false;
   message: string;
   ifPost = false;
-  socialNetworks :any= [];
+  socialNetworks: any = [];
   locale = 'pt-br';
   showPost: string;
   showInstagram: boolean = false;
-  form : FormGroup;
+  form: FormGroup;
   imageModal: string;
   textModal: string
   imageSrc: string;
@@ -37,102 +37,102 @@ export class AgendamentoComponent implements OnInit {
     private componentFactoryResolver: ComponentFactoryResolver,
     public viewContainerRef: ViewContainerRef,
     private localeService: BsLocaleService,
-    private agendamento :AgendamentoService,
+    private agendamento: AgendamentoService,
     private fb: FormBuilder,
     private modalService: BsModalService
-  ) { 
+  ) {
     localeService.use('pt-br');
-    
+
   }
 
   ngOnInit() {
     this.imageSrc = 'assets/img/img1.svg';
-   this.getSocial();
+    this.getSocial();
     this.form = this.fb.group({
-      data: [null, {required: true}],
+      data: [null, { required: true }],
       hora: [null],
-      text:[null, {required: true}],
-      img:[null], 
+      text: [null, { required: true }],
+      img: [null],
       social_network_key: new FormArray([])
-      });
-     
-}
-
-  getSocial(){
-   this.agendamento.getSocialNetworks().subscribe(
-     data => {
-      this.socialNetworks =  data
-      this.socialNetworks.forEach((o, i) => {
-         const control = new FormControl(false);
-        (this.form.controls.social_network_key as FormArray).push(control);
-        });
-
-     }
-   )
+    });
 
   }
 
-  handleSelection(event){
-    if(this.form.get('text').value === null){
-      this.form.get('text').setValue(  
-         event.char + '  '
-       )
-    }else{
-   this.form.get('text').setValue(  
-      this.form.get('text').value +' '+ event.char + ' '
-     )
-   }
+  getSocial() {
+    this.agendamento.getSocialNetworks().subscribe(
+      data => {
+        this.socialNetworks = data
+        this.socialNetworks.forEach((o, i) => {
+          const control = new FormControl(false);
+          (this.form.controls.social_network_key as FormArray).push(control);
+        });
+
+      }
+    )
+
+  }
+
+  handleSelection(event) {
+    if (this.form.get('text').value === null) {
+      this.form.get('text').setValue(
+        event.char + '  '
+      )
+    } else {
+      this.form.get('text').setValue(
+        this.form.get('text').value + ' ' + event.char + ' '
+      )
+    }
   }
 
   onFileChange(event) {
     const reader = new FileReader();
-    
-    if(event.target.files && event.target.files.length) {
+
+    if (event.target.files && event.target.files.length) {
       const [file] = event.target.files;
       reader.readAsDataURL(file);
-    
+
       reader.onload = () => {
-   
+
         this.imageSrc = reader.result as string;
-     
+
         this.form.patchValue({
           img: reader.result
         });
-   
+
       };
-   
+
     }
   }
 
-  save(){
-    if(this.form.valid){
-        this.ifError = false,
-        this.bsModalRef = this.modalService.show(ModalComponent, 
+  save() {
+    if (this.form.valid) {
+      this.ifError = false,
+        this.bsModalRef = this.modalService.show(ModalComponent,
           {
-            class: 'modal-dialog-centered' 
+            class: 'modal-dialog-centered'
           })
-    }  else{
-
+         } else { 
       this.ifError = true
-      
+    }
   }
-    
-    console.log(this.form.value)
 
-  }
+  openModal(data) {
+    if (this.form.valid) {
+      this.ifError = false;
+      const elementContainerRef = this.viewContainerRef;
+      elementContainerRef.clear();
+      const detail = elementContainerRef.createComponent(
+       this.componentFactoryResolver.resolveComponentFactory(DetalhesComponent));
+      detail.instance.postDetail = data;
+      detail.instance.image = this.imageSrc;
+      detail.instance.dataPost = data.data;
+      detail.instance.showLinkedin = data.social_network_key[1] === true;
+      detail.instance.showInstagram = data.social_network_key[2] === true;
+      detail.instance.modalClose = true;
+      detail.instance.isModal = 'isModal'
+         } else { 
+      this.ifError = true
+    }
   
-  openModal(data){
-    const elementContainerRef = this.viewContainerRef;
-    elementContainerRef.clear();
-
-     const detail  = elementContainerRef.createComponent(
-       this.componentFactoryResolver.resolveComponentFactory(DetalhesComponent) );
-         detail.instance.postDetail = data;  
-         detail.instance.image = this.imageSrc;
-         detail.instance.dataPost = data.data;
-         detail.instance.showLinkedin = data.social_network_key[1] === true;
-         detail.instance.showInstagram =  data.social_network_key[2] === true;
-         detail.instance.modalClose = true;
-         detail.instance.isModal = 'isModal'
   }
 }
