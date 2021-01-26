8b2b802b277e592c3875f3bb23b582f8572fb97a
cabukater <cabukater@gmail.com>
2021-01-26 06:35:44 -0300

    exprsss

---  server.js
+++  server.js
@@ -1,15 +1,12 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/mLabs'));
+app.use(express.static(`${__dirname}/dist/mLabs`));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/mLabs/'}),
-);
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/mLabs/index.html`));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
