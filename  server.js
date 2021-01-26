524bf9191d0a930a4f86e94d78b2e598e40f7c1a
cabukater <cabukater@gmail.com>
2021-01-26 06:50:19 -0300

    my first commit

---  server.js
+++  server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/mLabs`));
+app.use(express.static(`${__dirname}/dist/mlabs`));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/mLabs/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/mlabs/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
