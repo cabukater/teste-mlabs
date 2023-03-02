cef6b1f279bf82df31461d8264c61a27dbb8c7fd
camila assis <cabukater@gmail.com>
2023-03-02 16:55:45 -0300

    Update teste.ts

--- teste.ts
+++ teste.ts
@@ -1,25 +1,69 @@
-it('should emit the relations and charon session when a response is received', () => {
-  const relations = [{ rel: 'some_rel', href: 'some_href' }];
-  const charonSession = 'some_session';
-  const response = {
-    type: 'response',
-    headers: {
-      get: jest.fn().mockReturnValue(charonSession),
-    },
-  } as any;
+import { CaronteService, CharonService } from './caronte.service';
+import { CaronteFilter } from './caronte.filter';
 
-  charonServiceMock.initialize.mockReturnValue({
-    subscribe: jest.fn((callback) => {
-      callback(response);
-    }),
-  } as any);
-  charonServiceMock.getRelations.mockReturnValue(relations);
+describe('CaronteService', () => {
+  let caronteService: CaronteService;
+  let charonServiceMock: jest.Mocked<CharonService>;
 
-  const emitlistSpy = jest.spyOn(caronteService.emitList, 'next');
-  const getSessionSpy = jest.spyOn(caronteService.getSession, 'next');
+  beforeEach(() => {
+    charonServiceMock = {
+      initialize: jest.fn(),
+      getRelations: jest.fn(),
+    } as jest.Mocked<CharonService>;
 
-  caronteService.startCharon('some_token');
+    caronteService = new CaronteService(charonServiceMock);
+  });
 
-  expect(emitlistSpy).toHaveBeenCalledWith(relations);
-  expect(getSessionSpy).toHaveBeenCalledWith(charonSession);
+  describe('startCharon', () => {
+    // ...
+  });
+
+  describe('filter', () => {
+    let links: any[];
+    const rel = 'some_rel';
+    const met = 'some_met';
+
+    beforeEach(() => {
+      links = [
+        { rel: 'some_rel', href: 'some_href', method: 'get' },
+        { rel: 'some_rel', href: 'some_href', method: 'post' },
+        { rel: 'other_rel', href: 'other_href', method: 'get' },
+      ];
+    });
+
+    it('should emit the filtered links when at least one link matches the filter', () => {
+      const expectedFilteredLinks = [{ rel: 'some_rel', href: 'some_href', method: 'get' }];
+
+      const emitListSpy = jest.spyOn(caronteService.emitList, 'subscribe');
+      const getLinkSpy = jest.spyOn(caronteService.getLink, 'next');
+
+      caronteService.filter(rel, met);
+
+      expect(emitListSpy).toHaveBeenCalled();
+      expect(caronteService.resources).toEqual(links);
+      expect(caronteService.filteredLinks).toEqual(expectedFilteredLinks);
+      expect(getLinkSpy).toHaveBeenCalledWith(expectedFilteredLinks[0].href);
+    });
+
+    it('should set filteredLinks to an empty array if no links match the filter', () => {
+      const rel = 'rel';
+      const met = 'GET';
+      const resources: any[] = [
+        { href: 'https://example.com/foo', rel: 'rel', method: 'GET' },
+        { href: 'https://example.com/bar', rel: 'other', method: 'GET' },
+        { href: 'https://example.com/baz', rel: 'rel', method: 'POST' },
+      ];
+
+      const filter = new CaronteFilter(caronteService, charonServiceMock.initialize);
+      const emitListSpy = jest.spyOn(caronteService.emitList, 'subscribe').mockReturnValue(of(resources));
+      const getLinkSpy = jest.spyOn(caronteService.getLink, 'next');
+
+      filter.filter(rel, met);
+
+      expect(getLinkSpy).not.toHaveBeenCalled();
+      expect(filter.filteredLinks).toEqual([]);
+      emitListSpy.mockRestore();
+      getLinkSpy.mockRestore();
+    });
+  });
 });
