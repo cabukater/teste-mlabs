eaac9c87e73f52aed7b3b8534e0af76c535b4f8c
camila assis <cabukater@gmail.com>
2023-03-02 15:06:14 -0300

    Update teste.ts

--- teste.ts
+++ teste.ts
@@ -1,59 +1,19 @@
-describe('CaronteService', () => {
-  let caronteService: CaronteService;
-  let charonServiceMock: jest.Mocked<CharonService>;
+it('should emit the relations and charon session when a response is received', () => {
+  const relations = [{ rel: 'some_rel', href: 'some_href' }];
+  const charonSession = 'some_session';
+  const response = {
+    type: HttpEventType.Response,
+    headers: new HttpHeaders().set('x-charon-session', charonSession),
+  } as HttpResponse<any>;
 
-  beforeEach(() => {
-    charonServiceMock = {
-      initialize: jest.fn(),
-      getRelations: jest.fn(),
-    } as jest.Mocked<CharonService>;
+  charonServiceMock.initialize.mockReturnValue(of(response));
+  charonServiceMock.getRelations.mockReturnValue(relations);
 
-    caronteService = new CaronteService(charonServiceMock);
-  });
+  const emitlistSpy = jest.spyOn(caronteService.emitList, 'next');
+  const getSessionSpy = jest.spyOn(caronteService.getSession, 'next');
 
-  describe('startCharon', () => {
-    it('should call charonService.initialize with the correct parameters', () => {
-      const token = 'some_token';
+  caronteService.startCharon('some_token');
 
-      caronteService.startCharon(token);
-
-      expect(charonServiceMock.initialize).toHaveBeenCalledWith(
-        'charon_entrypoint',
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            'x-correlationID': expect.any(String),
-            apikey: 'some_apikey',
-          },
-          observe: 'response',
-        }
-      );
-    });
-
-    it('should emit the relations and charon session when a response is received', () => {
-      const relations = [{ rel: 'some_rel', href: 'some_href' }];
-      const charonSession = 'some_session';
-      const response = {
-        type: 'response',
-        headers: {
-          get: jest.fn().mockReturnValue(charonSession),
-        },
-      } as any;
-
-      charonServiceMock.initialize.mockReturnValue({
-        subscribe: jest.fn((callback) => {
-          callback(response);
-        }),
-      } as any);
-      charonServiceMock.getRelations.mockReturnValue(relations);
-
-      const emitlistSpy = jest.spyOn(caronteService.emitList, 'next');
-      const getSessionSpy = jest.spyOn(caronteService.getSession, 'next');
-
-      caronteService.startCharon('some_token');
-
-      expect(emitlistSpy).toHaveBeenCalledWith(relations);
-      expect(getSessionSpy).toHaveBeenCalledWith(charonSession);
-    });
-  });
+  expect(emitlistSpy).toHaveBeenCalledWith(relations);
+  expect(getSessionSpy).toHaveBeenCalledWith(charonSession);
 });
