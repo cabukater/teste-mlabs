c8cf6e8055a45a85dbea245f949bbb444f0afd1c
camila assis <cabukater@gmail.com>
2023-03-08 15:23:46 -0300

    Update teste.ts

--- teste.ts
+++ teste.ts
@@ -1,108 +1,27 @@
-import { HttpHeaders, HttpEventType } from '@angular/common/http';
-import { of } from 'rxjs';
-import { filter, map, switchMap } from 'rxjs/operators';
-import { CaronService } from '@minalib/caronService';
-import { CaronteService } from './CaronteService';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-describe('CaronteService', () => {
-  let service: CaronteService;
-  let caronService: jest.Mocked<CaronService>;
-
+describe('MinhaFuncao', () => {
   beforeEach(() => {
-    caronService = {
-      initialize: jest.fn(),
-      get: jest.fn(),
-      post: jest.fn(),
-    } as unknown as jest.Mocked<CaronService>;
-
-    service = new CaronteService(caronService);
-  });
-
-  describe('get', () => {
-    it('should call caronService.get with headers', () => {
-      const token = 'some-token';
-      const relation = 'some-relation';
-      const correlationId = 'some-correlation-id';
-      const endpoint = 'some-endpoint';
-      const key = 'some-key';
-      const body = { some: 'body' };
-
-      const headers = new HttpHeaders({
-        aut: `Bearer ${token}`,
-        'id-cor': correlationId,
-        key,
-      });
-
-      const response = {
-        type: HttpEventType.Response,
-      };
-
-      caronService.initialize.mockReturnValue(
-        of(response as any)
-      );
-      caronService.get.mockReturnValue(
-        of({ type: HttpEventType.Response, body } as any)
-      );
-
-      service.get(token, relation, correlationId).subscribe((result) => {
-        expect(caronService.initialize).toHaveBeenCalledWith(
-          endpoint,
-          expect.objectContaining({
-            headers,
-            observe: 'response',
-          })
-        );
-        expect(caronService.get).toHaveBeenCalledWith(relation, {
-          headers,
-        });
-        expect(result).toEqual(body);
-      });
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
     });
   });
 
-  describe('post', () => {
-    it('should call caronService.post with headers and body', () => {
-      const token = 'some-token';
-      const relation = 'some-relation';
-      const correlationId = 'some-correlation-id';
-      const endpoint = 'some-endpoint';
-      const key = 'some-key';
-      const body = { some: 'body' };
+  it('deve tratar erros de acesso negado', () => {
+    const error = { status: 403 };
+    const loaderProfile = {
+      denyAccess: jasmine.createSpy('denyAccess')
+    };
+    const router = {
+      navigateByUrl: jasmine.createSpy('navigateByUrl')
+    };
 
-      const headers = new HttpHeaders({
-        aut: `Bearer ${token}`,
-        'id-cor': correlationId,
-        key,
-      });
+    const minhaFuncao = new MinhaFuncao(loaderProfile, router);
 
-      const response = {
-        type: HttpEventType.Response,
-        ok: true,
-        body: JSON.stringify(body),
-      };
+    minhaFuncao.handleError(error);
 
-      caronService.initialize.mockReturnValue(
-        of(response as any)
-      );
-      caronService.post.mockReturnValue(
-        of(response as any)
-      );
-
-      service.post(token, relation, correlationId, body).subscribe((result) => {
-        expect(caronService.initialize).toHaveBeenCalledWith(
-          endpoint,
-          expect.objectContaining({
-            headers,
-            observe: 'response',
-          })
-        );
-        expect(caronService.post).toHaveBeenCalledWith(relation, {
-          headers,
-          observe: 'body',
-          responseType: 'text',
-        });
-        expect(result).toEqual(body);
-      });
-    });
+    expect(loaderProfile.denyAccess).toHaveBeenCalled();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('negado', error);
   });
 });
